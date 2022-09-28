2f73fc66611dc9ffee17208b7c99d6baf8db13e0
Peter Wolf <pewobox@Peters-Air.fritz.box>
2022-09-28 05:50:07 +0200

    first commit

---
+++ tag2/js/dom.js
@@ -0,0 +1,13 @@
+
+const myElement = document.getElementById("sideNavigation");
+const myClasses = document.getElementsByClassName("cardItem");
+const myQuerySelector = document.querySelectorAll("div")
+const myClassQuery = document.querySelectorAll(".cardItem")
+
+console.log(myElement)
+
+console.log(myClasses)
+
+console.log(myQuerySelector)
+
+console.log(myClassQuery)
---
+++ tag2/js/main.js
@@ -0,0 +1,24 @@
+//das ist die konstante
+const VOR_NAME = "peter";
+//variable
+let vorname = "peter";
+
+//integer - float
+const numConstant = 20;
+
+//string
+const numString = "20.5";
+
+//boolean - true or false
+const isLoading = true
+
+
+//array
+const myArray = ["string", 255, {name: "klaus", street: "bei ihm"}]
+
+//object
+const myObject = {
+    name: "peter",
+    birthdate: "23.6.1900",
+    street: "daheim"
+}
